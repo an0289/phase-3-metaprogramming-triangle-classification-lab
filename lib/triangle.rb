d20b20e481e8ece5c219fe9ca0c25c72016abe9b
Alexandra <alexandraneff@live.com>
2023-01-11 20:26:56 -0600

    commit tree fabc8ef4cf21bce479fe265362e0cd862f2bbd9c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+attr_reader :a, :b, :c
+
+  def initialize (a, b, c)
+    @a = a 
+    @b = b 
+    @c = c 
+  end 
+
+  def kind 
+    validate_triangle
+    if a == b && b == c  
+      :equilateral
+    elsif a == b  || b == c || a == c 
+      :isosceles 
+    else 
+      :scalene
+    end
+  end 
+
+  def is_valid_triangle?  
+    [a,b,c].all? {|n| n > 0} && (a + b > c && b + c > a && a + c > b)
+  end 
+
+  def validate_triangle
+    raise TriangleError unless is_valid_triangle? 
+  end 
+
+  class TriangleError < StandardError
+  end 
 end
